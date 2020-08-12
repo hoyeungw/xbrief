ce7a79f0dad0034bf0c71e15fd85598530e7f8f7
hazen <hoyeungw@outlook.com>
2020-08-12 18:34:48 +0800

    init commit

---
+++ test/assets/typen_testables/numerics.py
@@ -0,0 +1,11 @@
+numerics = {
+  'num zero': 0,
+  'num one': 1,
+  'num frac': .42,
+  'num positive': 1024,
+  'num negative': -1024,
+    # 'num EPSILON': Number.EPSILON,
+    # 'num NaN': Number.NaN,
+    # 'num POS_INF': Number.POSITIVE_INFINITY,
+    # 'num NEG_INF': Number.NEGATIVE_INFINITY,
+}
